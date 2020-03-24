6343adba34e64e72c29b13d2cd53d112976b5562
ants <ants@newtime.ee>
2020-03-24 20:31:00 +0200

    Added main-bg, with global stying. Added illustrations.

--- src/styledComponents/GlobalStyle.js
+++ src/styledComponents/GlobalStyle.js
@@ -1,7 +1,13 @@
 import { createGlobalStyle } from "styled-components";
+import mainBgImage from "../assets/images/main-bg.jpg";
 
 export const GlobalStyle = createGlobalStyle`
   body, html {
     height: 100%
+  }
+  body {
+    background-image: url(${mainBgImage});
+    background-repeat: no-repeat;
+    background-size: cover;
   }
 `;
