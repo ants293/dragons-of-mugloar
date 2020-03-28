b5efb3afefe1ab685f7a77e0ef6ed5214c54f063
ants <ants@newtime.ee>
2020-03-28 11:42:01 +0200

    Put illustrations and styles on map.

--- src/views/Map/Map.jsx
+++ src/views/Map/Map.jsx
@@ -3,10 +3,14 @@ import "./style.scss";
 
 function MapView() {
   return (
-    <div className="map">
-      <div className="illustration_1" />
-      <div className="illustration_2" />
-      <div className="illustration_3" />
+    <div className="map h-full">
+      <div className="container relative h-full">
+        <div className="illustrations d-flex h-full flex-wrap align-content-end justify-content-between">
+          <div className="illustration__one" />
+          <div className="illustration__three" />
+          <div className="illustration__two" />
+        </div>
+      </div>
     </div>
   );
 }
