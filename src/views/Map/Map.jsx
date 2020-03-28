3e7ef9da262c72db6c0b4db3608b966b89ee7efe
ants <ants@newtime.ee>
2020-03-28 12:47:40 +0200

    Added svg hovers.

--- src/views/Map/Map.jsx
+++ src/views/Map/Map.jsx
@@ -3,12 +3,12 @@ import "./style.scss";
 
 function MapView() {
   return (
-    <div className="map h-full">
+    <div className="map relative h-full">
       <div className="container relative h-full">
         <div className="illustrations d-flex h-full flex-wrap align-content-end justify-content-between">
-          <div className="illustration__one" />
-          <div className="illustration__three" />
-          <div className="illustration__two" />
+          <div className="illustration__item illustration__one" />
+          <div className="illustration__item illustration__three align-self-end" />
+          <div className="illustration__item illustration__two" />
         </div>
       </div>
     </div>
