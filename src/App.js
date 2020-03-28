4750054c7bc411d53ae72c68ef1f7f6ded180d31
ants <ants@newtime.ee>
2020-03-28 10:16:31 +0200

    Added boostrap grid.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes/Routes";
+import "./assets/styles/css-modules/bootstrap-grid.min.css";
+import "./assets/styles/css-modules/normalize.css";
 import "./assets/styles/index.scss";
 
 function App() {
