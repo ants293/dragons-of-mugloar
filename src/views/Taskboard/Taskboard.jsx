296d4b11b16d8bba153011e1455d947ac05a07be
ants <ants@newtime.ee>
2020-03-29 23:43:10 +0300

    Sorted the noticeboard list by task difficulty and price.

--- src/views/Taskboard/Taskboard.jsx
+++ src/views/Taskboard/Taskboard.jsx
@@ -23,11 +23,15 @@ function TaskboardView({
     }
   });
 
+  const sortedTaskList = tasks.taskList.sort((a, b) =>
+    sortTasksByDifficultyAndPrice(a, b)
+  );
+
   return (
     <MainViewWrapper title="Noticeboard">
       <div className="col-12">
         <div className="row items-wall flex-wrap">
-          {tasks.taskList.map((task) => (
+          {sortedTaskList.map((task) => (
             <TaskCard
               key={task.adId}
               task={task}
@@ -43,6 +47,32 @@ function TaskboardView({
     </MainViewWrapper>
   );
 }
+
+const probabilityLevels = {
+  "Piece of cake": 1,
+  "Walk in the park": 2,
+  "Sure thing": 3,
+  "Quite likely": 4,
+  Gamble: 5,
+  "Hmmm....": 6,
+  "Rather detrimental": 7,
+  Risky: 8,
+  "Suicide mission": 9,
+};
+
+const sortTasksByDifficultyAndPrice = (a, b) => {
+  if (probabilityLevels[a.probability] < probabilityLevels[b.probability]) {
+    return -1;
+  }
+  if (
+    probabilityLevels[a.probability] === probabilityLevels[b.probability] &&
+    a.reward < b.reward
+  ) {
+    return -1;
+  }
+
+  return 0;
+};
 
 const throwTaskSolvingToast = (taskPassed) => {
   if (taskPassed) {
