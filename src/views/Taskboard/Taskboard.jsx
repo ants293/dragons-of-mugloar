7570f8b665dd72182d09bda7b12651b9f38980eb
ants <ants@newtime.ee>
2020-03-29 15:15:55 +0300

    Taskboard tasklist html.

--- src/views/Taskboard/Taskboard.jsx
+++ src/views/Taskboard/Taskboard.jsx
@@ -2,19 +2,39 @@ import React, { useEffect } from "react";
 import "./style.scss";
 import MainViewWrapper from "../../components/Wrapper";
 
-function TaskboardView({ actionRequestTaskList = () => {}, tasks = [] }) {
+function TaskboardView({
+  actionRequestTaskList = () => {},
+  tasks = { taskList: [] },
+}) {
   useEffect(() => {
     actionRequestTaskList();
   }, []);
 
-  console.log(tasks);
-
   return (
     <MainViewWrapper title="Available jobs">
-      <div className="col-5">
-        <div>some illustration</div>
-      </div>
-      <div className="col-7 items-list">
+      <div className="col-12 items-wall d-flex flex-wrap">
+        {tasks.taskList.map((task) => (
+          <div className="items-wall__item task-card">
+            <div className="task-card__header">
+              <span className="task-card__header-item">
+                <span>Reward:</span>
+                {task.reward}
+              </span>
+              <span className="task-card__header-item">
+                <span>Difficulty:</span>
+                {task.probability}
+              </span>
+              <span className="task-card__header-item">
+                <span>Expires in:</span>
+                {task.expiresIn}
+              </span>
+            </div>
+            <div className="task-card__content">{task.message}</div>
+            <div className="task-card__footer">
+              <button className="main-button">Do it!</button>
+            </div>
+          </div>
+        ))}
         <div>list here</div>
       </div>
     </MainViewWrapper>
