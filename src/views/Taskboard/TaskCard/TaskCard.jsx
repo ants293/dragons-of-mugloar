ef354ea25c482377fbc6acc511cb20e9adeda647
ants <ants@newtime.ee>
2020-03-29 23:16:38 +0300

    Split Taskboards cards into separate component.

---
+++ src/views/Taskboard/TaskCard/TaskCard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function TaskCard({task, onTaskClick}) {
+  return (
+    <div className="col-4 task-card">
+      <div className="items-wall__item">
+        <div className="task-card__header mb-2">
+          <span className="task-card__header-item">
+            <span className="bold mr-1 task-card__header-col">Reward:</span>
+            <span className="task-card__header-col">{task.reward} gold</span>
+          </span>
+          <span className="task-card__header-item">
+            <span className="bold mr-1 task-card__header-col">Difficulty:</span>
+            <span className="task-card__header-col">{task.probability}</span>
+          </span>
+          <span className="task-card__header-item">
+            <span className="bold mr-1 task-card__header-col">Expires in:</span>
+            <span className="task-card__header-col">
+              {task.expiresIn} turns
+            </span>
+          </span>
+        </div>
+        <div className="task-card__content mb-3">{task.message}</div>
+        <div className="task-card__footer">
+          <button
+            className="main-button"
+            onClick={() => onTaskClick()}
+          >
+            Do job
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TaskCard;
--- src/views/Taskboard/Taskboard.jsx
+++ src/views/Taskboard/Taskboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./style.scss";
 import MainViewWrapper from "../../components/Wrapper/Wrapper";
 import { toast } from "react-toastify";
+import TaskCard from "./TaskCard/TaskCard";
 
 function TaskboardView({
   actionRequestTaskList = () => {},
@@ -27,49 +28,15 @@ function TaskboardView({
       <div className="col-12">
         <div className="row items-wall flex-wrap">
           {tasks.taskList.map((task) => (
-            <div key={task.adId} className="col-4 task-card">
-              <div className="items-wall__item">
-                <div className="task-card__header mb-2">
-                  <span className="task-card__header-item">
-                    <span className="bold mr-1 task-card__header-col">
-                      Reward:
-                    </span>
-                    <span className="task-card__header-col">
-                      {task.reward} gold
-                    </span>
-                  </span>
-                  <span className="task-card__header-item">
-                    <span className="bold mr-1 task-card__header-col">
-                      Difficulty:
-                    </span>
-                    <span className="task-card__header-col">
-                      {task.probability}
-                    </span>
-                  </span>
-                  <span className="task-card__header-item">
-                    <span className="bold mr-1 task-card__header-col">
-                      Expires in:
-                    </span>
-                    <span className="task-card__header-col">
-                      {task.expiresIn} turns
-                    </span>
-                  </span>
-                </div>
-                <div className="task-card__content mb-3">{task.message}</div>
-                <div className="task-card__footer">
-                  <button
-                    className="main-button"
-                    onClick={() =>
-                      actionRequestTaskSolving(task.adId).then((taskPassed) =>
-                        throwTaskSolvingToast(taskPassed)
-                      )
-                    }
-                  >
-                    Do job
-                  </button>
-                </div>
-              </div>
-            </div>
+            <TaskCard
+              key={task.adId}
+              task={task}
+              onTaskClick={() =>
+                actionRequestTaskSolving(task.adId).then((taskPassed) =>
+                  throwTaskSolvingToast(taskPassed)
+                )
+              }
+            />
           ))}
         </div>
       </div>
